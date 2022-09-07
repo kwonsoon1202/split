47caf492d1798bae56ff0e75de2848604067b29d
kwon <skytotheblue7@gmail.com>
2022-09-07 14:18:41 +0900

    split

--- main.cpp
+++ main.cpp
@@ -7,6 +7,19 @@ using std::endl;
 using std::string;
 using std::vector;
 
+vector<string> split(const string& s)
+
+   vector<string> words;
+    string::size_type i = 0;
+    while (i != s.szie()){
+        while() {
+            ++i;
+        }
+
+    }
+   return words;
+}
+
 int main() {
 
     return 0;
