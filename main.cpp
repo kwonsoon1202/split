3b5585b82950f6ef098db3f210cc8c5e548325fe
kwon <skytotheblue7@gmail.com>
2022-09-07 14:21:25 +0900

    split

--- main.cpp
+++ main.cpp
@@ -12,10 +12,14 @@ vector<string> split(const string& s)
    vector<string> words;
     string::size_type i = 0;
     while (i != s.szie()){
-        while() {
+        while(i != s.size() && isspace(s[i])) {
             ++i;
         }
+        string::size_type j = i;
+        while(j != s.szie() && !isspace(s[j])){
+            ++j;
 
+        }
     }
    return words;
 }
